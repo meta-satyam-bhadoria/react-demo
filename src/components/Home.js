7150053a01247f1b8a49e2996db1b9a783b4357d
Satyam Bhadoria <satyam.bhadoria@metacube.com>
2018-04-17 17:52:23 +0530

    Deleted as structure to be change

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Switch, Link, BrowserRouter as Router, Route } from 'react-router-dom';
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
-export default Home;
--- src/scripts/play.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  //alert("hello satyam. I'm called from jQuery.");
-});
